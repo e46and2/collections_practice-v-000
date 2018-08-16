215f16d19b69b87904eb14768f7d7d50b3c987b8
Emily Nicholl Liepe <e.nicholl.liepe@gmail.com>
2018-08-16 17:50:16 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,28 @@
+ def sort_array_asc(array)
+  array.sort 
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort_by(&:length)
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |string| string.split('').map.to_a 
+  string[2] = "$"
+  string
+end
+end
+end
